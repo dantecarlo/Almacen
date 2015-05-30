94a6984f4fcecfc57336076757f23580abee1d64
luisa <luisa.nunez@ucsp.edu.pe>
2015-05-30 01:21:59 -0500

    modificar ProviderController

--- app/Controller/ProviderController.php
+++ app/Controller/ProviderController.php
@@ -7,25 +7,16 @@ class ProviderController extends AppController{
 
 
 	public function index(){
-		//$this->set('Orden_de_compra', $this->PurchaseOrder->find('all'));
 		$this -> Provider -> recursive = 0;
 		$Provider = $this -> paginate();
 		if($this->request->is('request')):
 			return $Provider;
 		else:
-			$this-> set('Provider', $Provider);
+			$this->set('Provider', $Provider);
 		endif;
 	}
 
-/*
 
-	public function index(){
-		//$this->set('Orden_de_compra', $this->PurchaseOrder->find('all'));
-		$this -> Provider -> recursive = 0;
-		$Providers = $this -> paginate();
-		$this-> set('Provider', $Providers);
-	}
-*/
 	public function add(){
 		if($this->request->is('post')):
 			if( $this->Provider->save($this->request->data) ):
